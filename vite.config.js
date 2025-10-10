5f0d89a737c6cad48dc91881232b186770228ee0
Federico Lyonnet <fedelyonnet@gmail.com>
2025-10-09 23:09:43 -0300

    Fix Vite CORS for stroke images and set specific beneficios background color for carreras page

--- vite.config.js
+++ vite.config.js
@@ -14,4 +14,13 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    server: {
+        cors: true,
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+            'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+        },
+    },
+    publicDir: 'public',
 });
